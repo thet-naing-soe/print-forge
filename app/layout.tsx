357529a8973eab13463fc4532127f2838ad3950a
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-19 11:41:15 +0630

    add header to print forge

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              {/* Desktop logo */}
+              <img
+                src="/printforge-logo.svg"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block"
+              />
+              {/* Mobile logo */}
+              <img
+                src="/printforge-logo-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
