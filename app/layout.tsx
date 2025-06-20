969e3178e73d524d27e4c1895b0c59951884ad29
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-20 14:16:21 +0630

    Link in nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
 import Image from 'next/image'
+import Link from 'next/link'
 import PFLogoIcon from "@/public/printforge-logo-icon.svg"
 import PFLogo from "@/public/printforge-logo.svg"
 
@@ -26,10 +27,12 @@ export default function RootLayout({
       <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
-            <div className="relative">
+
+            <Link href='/'>
+              <div className="relative">
               {/* Desktop Logo */}
               <Image
-                src={PFLogoIcon}
+                src={PFLogo}
                 alt="PrintForge Logo"
                 className="w-[200px] h-auto hidden md:block"
               />
@@ -40,9 +43,10 @@ export default function RootLayout({
                 className="w-[40px] h-auto block md:hidden"
               />
             </div>
+            </Link>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p href="/about">About</p>
+              <li className="text-sm uppercase cursor-pointer"><Link href="/">3D Models</Link></li>
+              <li className="text-sm uppercase cursor-pointer"><Link href="/about">About</Link></li>
             </ul>
           </nav>
         </header>
