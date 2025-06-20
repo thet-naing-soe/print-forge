c726224804cba63909c225255fa69831857a112b
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-20 17:00:55 +0630

    add model detail page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import "./globals.css";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
-import Image from 'next/image'
-import Link from 'next/link'
 import type {RootLayoutProps} from "@/app/types"
-import PFLogoIcon from "@/public/printforge-logo-icon.svg"
-import PFLogo from "@/public/printforge-logo.svg"
+import Navbar from "@/app/components/Navbar"
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -24,31 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
-        <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4">
-
-            <Link href='/'>
-              <div className="relative">
-              {/* Desktop Logo */}
-              <Image
-                src={PFLogo}
-                alt="PrintForge Logo"
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/* Mobile Logo */}
-              <Image
-                src={PFLogoIcon}
-                alt="PrintForge Logo"
-                className="w-[40px] h-auto block md:hidden"
-              />
-            </div>
-            </Link>
-            <ul className="flex items-center gap-2.5">
-              <li className="text-sm uppercase cursor-pointer"><Link href="/3d-models">3D Models</Link></li>
-              <li className="text-sm uppercase cursor-pointer"><Link href="/about">About</Link></li>
-            </ul>
-          </nav>
-        </header>
+      <Navbar />
         {children}
       </body>
     </html>
